2ee6d1d98ba86cd65da589bfeb4ee53e2b0e7f8d
Noelle Ip <noelle.ip@superhub.com.hk>
2023-02-10 15:29:20 +0800

    refactor: refactor updateQuality loop structure

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -18,24 +18,18 @@ class Shop {
 
     for (let item of this.items) {
       if (item.name != agedBrie && item.name != backStage) {
-        if (item.quality > 0) {
-          if (item.name != sulfuras) {
+        if (item.quality > 0 && item.name != sulfuras) {
             item.quality = item.quality - 1;
-          }
         }
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
           if (item.name == backStage) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
+            if (item.sellIn < 11 && item.quality < 50) {
                 item.quality = item.quality + 1;
-              }
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
+            if (item.sellIn < 6 && item.quality < 50) {
                 item.quality = item.quality + 1;
-              }
             }
           }
         }
@@ -45,14 +39,10 @@ class Shop {
       }
       if (item.sellIn < 0) {
         if (item.name != agedBrie) {
-          if (item.name != backStage) {
-            if (item.quality > 0) {
-              if (item.name != sulfuras) {
-                item.quality = item.quality - 1;
-              }
-            }
+          if (item.name != backStage && item.name != sulfuras && item.quality > 0) {
+              item.quality = item.quality - 1;
           } else {
-            item.quality = item.quality - item.quality;
+              item.quality = item.quality - item.quality;
           }
         } else {
           if (item.quality < 50) {
