606839ad734b56647575b67f700e7a1fa5e58839
Noelle Ip <noelle.ip@superhub.com.hk>
2023-02-10 17:05:03 +0800

    Refactor: replaced hardcode number with variables

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -9,12 +9,31 @@ class Item {
 class Shop {
   constructor(items=[]){
     this.items = items;
+    this.minQuality = 0;
+    this.maxQuality = 50;
+    this.increasedQuality = 1;
+    this.decreasedQuality = 1;
+    this.decreasedSellIn = 1;
+    this.firstDayLimit = 11;
+    this.secondDayLimit = 6;
+    this.thirdDayLimit = 0;
   }
+
   updateQuality() {
     let agedBrie = 'Aged Brie';
     let backStage = 'Backstage passes to a TAFKAL80ETC concert';
     let sulfuras = 'Sulfuras, Hand of Ragnaros';
-
+    let minQuality = this.minQuality;
+    let maxQuality = this.maxQuality;
+    let increasedQuality = this.increasedQuality;
+    let decreasedQuality = this.decreasedQuality;
+    let decreasedSellIn = this.decreasedSellIn;
+    let firstDayLimit = this.firstDayLimit;
+    let secondDayLimit = this.secondDayLimit;
+    let thirdDayLimit = this.thirdDayLimit;
+    
+    // categorise these three items (by category to deal with)
+    // extract method
 
     for (let item of this.items) {
       let name = item.name;
@@ -22,35 +41,37 @@ class Shop {
       let quality = item.quality;
 
       if (name != agedBrie && name != backStage) {
-        if (quality > 0 && name != sulfuras) {
-            item.quality = item.quality - 1;
+        if (quality > minQuality && name != sulfuras) {
+                item.quality -= decreasedQuality;
         }
       } else {
-        if (quality < 50) {
-          item.quality = item.quality + 1;
+        if (quality < maxQuality) {
+                item.quality += increasedQuality;
           if (name == backStage) {
-            if (sellIn < 11 && quality < 50) {
-                item.quality = item.quality + 1;
+            if (sellIn < firstDayLimit && quality < maxQuality) {
+                item.quality += increasedQuality;
             }
-            if (sellIn < 6 && quality < 50) {
-                item.quality = item.quality + 1;
+            if (sellIn < secondDayLimit && quality < maxQuality) {
+                item.quality += increasedQuality;
             }
+            // how to minimise change cost of dayLimit(s)?
           }
         }
       }
       if (name != sulfuras) {
-        item.sellIn = item.sellIn - 1;
+                item.sellIn -= decreasedSellIn;
       }
-      if (sellIn < 0) {
+      if (sellIn < thirdDayLimit) 
+      {
         if (name != agedBrie) {
-          if (name != backStage && name != sulfuras && quality > 0) {
-              item.quality = item.quality - 1;
+          if (name != backStage && name != sulfuras && quality > minQuality) {
+                item.quality -= decreasedQuality;
           } else {
-              item.quality = item.quality - item.quality;
+                item.quality -= item.quality;
           }
         } else {
-          if (quality < 50) {
-            item.quality = item.quality + 1;
+          if (quality < maxQuality) {
+                item.quality += increasedQuality;
           }
         }
       }
